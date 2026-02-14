8d91cc3cef6a27e81f67e6e0f540b1a6ec008fd8
0xRektified <bugislife@proton.me>
2026-02-14 17:15:56 +0800

    feat: finish core program before test

--- programs/anchor-dice-game-q4-25/src/instructions/resolve_bet.rs
+++ programs/anchor-dice-game-q4-25/src/instructions/resolve_bet.rs
@@ -1,7 +1,9 @@
-use anchor_lang::{prelude::*};
+use anchor_lang::{prelude::*, system_program::{Transfer, transfer}};
 use anchor_instruction_sysvar::Ed25519InstructionSignatures;
-use solana_program::{ed25519_program, example_mocks::solana_sdk::signature, sysvar::instructions::load_instruction_at_checked};
+use solana_program::{ed25519_program, sysvar::instructions::load_instruction_at_checked, hash::hash};
 use crate::{errors::DiceError, state::Bet};
+
+pub const HOUSE_EDGE: u16 = 150;
 
 #[derive(Accounts)]
 pub struct ResolveBet<'info>{
@@ -64,10 +66,56 @@ impl<'info> ResolveBet<'info> {
             DiceError::Ed25519Signature
         );
 
+        require!(
+            &signature.message.as_ref().ok_or(DiceError::Ed25519Signature)?.eq(&self.bet.to_slice()),
+            DiceError::Ed25519Message
+        );
         Ok(())
     }
 
     pub fn resolve_bet(&mut self, bumps: &ResolveBetBumps, sig: &[u8]) -> Result<()> {
+        let hash = hash(sig).to_bytes();
+        let mut hash_16: [u8;16] = [0;16];
+        hash_16.copy_from_slice(&hash[0..16]);
+
+        let lower = u128::from_le_bytes(hash_16);
+
+        hash_16.copy_from_slice(&hash[16..32]);
+        let upper = u128::from_le_bytes(hash_16);
+
+        let roll = lower
+            .wrapping_add(upper)
+            .wrapping_rem(100) as u8 + 1;
+
+        if self.bet.roll > roll {
+            let payout = (self.bet.amount as u128)
+                .checked_mul(10000 - HOUSE_EDGE as u128)
+                .ok_or(DiceError::Overflow)?
+                .checked_div(self.bet.roll as u128 - 1)
+                .ok_or(DiceError::Overflow)?
+                .checked_div(100)
+                .ok_or(DiceError::Overflow)?;
+
+            let signer_seeds: &[&[&[u8]]] = &[&[
+                b"vault",
+                &self.house.key().to_bytes(),
+                &[bumps.vault]
+            ]];
+
+            let accounts = Transfer{
+                from: self.vault.to_account_info(),
+                to: self.player.to_account_info(),
+            };
+
+            let ctx = CpiContext::new_with_signer(
+                self.system_program.to_account_info(),
+                accounts,
+                signer_seeds,
+            );
+
+            transfer(ctx, payout as u64)?;
+
+        }
         Ok(())
     }
 }
