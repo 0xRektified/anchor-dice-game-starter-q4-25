931546d30a9fe9b708aa899d376110703fd8707b
0xRektified <bugislife@proton.me>
2026-02-14 16:31:31 +0800

    fix: resolve_bet fonction signature

--- programs/anchor-dice-game-q4-25/src/instructions/resolve_bet.rs
+++ programs/anchor-dice-game-q4-25/src/instructions/resolve_bet.rs
@@ -9,12 +9,12 @@ pub struct ResolveBet<'info>{
 }
 
 impl<'info> ResolveBet<'info> {
-    pub fn verify_ed25519_signature(&mut self, sig: &Vec<u8>) -> Result<()> {
+    pub fn verify_ed25519_signature(&mut self, sig: &[u8]) -> Result<()> {
         // TODO
         Ok(())
     }
 
-    pub fn resolve_bet(&mut self, bumps: &ResolveBetBumps, sig: &Vec<u8>) -> Result<()> {
+    pub fn resolve_bet(&mut self, bumps: &ResolveBetBumps, sig: &[u8]) -> Result<()> {
         // TODO
         Ok(())
     }
